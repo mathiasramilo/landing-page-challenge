962c78b8d63900c73f53c07721d02ebaf4288750
Mathias Ramilo <mathias.ramilo@lightit.io>
2024-09-12 17:44:11 -0300

    feat(landing page): finish desktop version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-  purge: [
-    'index.html',
-  ],
+  purge: ['index.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {
+      boxShadow: {
+        '3xl': '16px 16px 60px -15px rgba(0, 0, 0, 0.05)',
+      },
     },
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
